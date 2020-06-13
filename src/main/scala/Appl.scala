6495b94f0dd1a363c520997d38a7a28576f35101
Filip Twardy <filip.twardy.v01@gmail.com>
2020-06-13 20:53:24 +0200

    Simple example to run in intellij

--- src/main/scala/Appl.scala
+++ src/main/scala/Appl.scala
@@ -2,6 +2,16 @@
 object Appl {
   def main(args: Array[String]): Unit = {
     val client = new Client
-    client.run
+    val epochs : Int = 100
+
+    for (_ <- 1 to epochs){
+
+      var envData : String = client.sendAction("random")
+      println(envData)
+
+    }
+
+    client.sendAction("kill")
+
   }
 }
