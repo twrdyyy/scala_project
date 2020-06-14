6f526fc2491b7349aa784679d5cb1c8c9bc0caf5
Filip Twardy <filip.twardy.v01@gmail.com>
2020-06-14 13:44:38 +0200

    Layer tests

--- src/main/scala/Appl.scala
+++ src/main/scala/Appl.scala
@@ -13,7 +13,15 @@ object Appl {
 //    client.sendAction("kill")
 //    val model = new Model(Array("3 relu", "4 softmax"), input_size = 2)
 
-    val x = ns.ones(3, 3)
-    print(ns.dot(ns.sum(x, axis=1).T, x))
+    val l = new Layer(neurons = 4, input_size = 4, activation = "relu")
+    val x = Tensor(1, 2, 3, 4).T
+    println(l.forward(x))
+    val grad = ns.rand(4, 4)
+    val Array(dA, dB) = l.backward(grad, x)
+    val lr = 0.001f
+    println(l.A)
+    println(l.B)
+    println(l.A - dA * lr)
+    println(l.B - dB * lr)
   }
 }
--- src/main/scala/Layer.scala
+++ src/main/scala/Layer.scala
@@ -3,8 +3,8 @@ import ns.Tensor
 
 class Layer(neurons : Int, input_size : Int, activation : String, random_init : Boolean = true, bias_init : Float = 0.0f) {
 
-  var A: Tensor = ns.rand(input_size, neurons)
-  var B: Tensor = ns.zeros(neurons, 1)
+  var A: Tensor = _
+  var B: Tensor = ns.full(Array(neurons), bias_init)
   var f : ActivationFunction = _
   var z : ns.Tensor = _
 
@@ -13,12 +13,18 @@ class Layer(neurons : Int, input_size : Int, activation : String, random_init : 
     case "sigmoid" => f = Sigmoid
   }
 
+  random_init match {
+    case true => A = ns.rand(input_size, neurons)
+    case false => A = ns.zeros(input_size, neurons)
+  }
+
+
   def forward (x : Tensor) : Tensor = {
     z = ns.dot(A, x) + B
     f(ns.copy(z))
   }
 
-  def backward (grad : Tensor, x : Tensor, y : Tensor): Array[Tensor] = {
+  def backward (grad : Tensor, x : Tensor): Array[Tensor] = {
     val Array(_, m) = x.shape
     val dA = (1 / m) * ns.multiply(grad, f(z).T)
     val dB = (1 / m) * ns.sum(grad, axis = 1).T
